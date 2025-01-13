0fb205ed1a0f6b4e44a0d085e6721280d8afe8f9
FilipSl3 <filip.slemp7@gmail.com>
2025-01-13 21:20:49 +0100

    Update +page.svelte

--- src/routes/main/admin/warehouse/+page.svelte
+++ src/routes/main/admin/warehouse/+page.svelte
@@ -126,7 +126,7 @@
 				<div class="item">
 					<h3>{item.item_name}</h3>
 					<p>Quantity: {item.quantity}</p>
-					<p>Arrival Date: {item.arrival_date}</p>
+					<p>Arrival Date: {new Date(item.arrival_date).toLocaleDateString()}</p>
 					<p>Added By: {item.added_by}</p>
 					<p>Warehouse ID: {item.warehouse_id}</p>
 				</div>
